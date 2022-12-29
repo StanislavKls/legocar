6424bd60ac9bb78e3c4e5b42c2bc3bf01b3d575a
StanislavKls <73436574+StanislavKls@users.noreply.github.com>
2022-12-29 07:49:52 +0500

    Delete db_config.php

--- config/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-            'host' => 'localhost',
-            'port' => '5432',
-            'database' => 'legocar',
-            'username' => 'postgres',
-            'password' => 'klecov',
-        ];
